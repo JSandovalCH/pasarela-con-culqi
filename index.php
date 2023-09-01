18cbb2c46082abec71301b8a0a7172a905071640
Jeanpier RR <jeanpierrojasramos4@gmail.com>
2023-09-01 16:20:38 -0500

    aceptando cambios del index

--- index.php
+++ index.php
@@ -10,46 +10,16 @@
   </head>
   <body>
     <h1>Hola Mundo</h1>
-    <div class="card">
-  <div class="card-header">
-    Pago con Culqui
-  </div>
-  <div class="card-body">
-    <div class="card-body">
-        <div class="row">
-          <button class="btn btn-success" id="btn_pagar"></button>
-        </div>
-    </div>
-  </div>
-</div>
     <script>
     Culqi.publicKey = 'pk_test_07e2b9f728f921sd';
 
-    
-
-  /*Culqi.options({
-      style: {
-        logo: 'https://culqi.com/LogoCulqi.png',
-        bannerColor: '', // hexadecimal
-        buttonBackground: '', // hexadecimal
-        menuColor: '', // hexadecimal
-        linksColor: '', // hexadecimal
-        buttonText: '', // texto que tomará el botón
-        buttonTextColor: '', // hexadecimal
-        priceColor: '' // hexadecimal
-      }
-  });*/
-
-  const btn_pagar = document.getElementById('btn_pagar');
-
-  btn_pagar.addEventListener('click', function (e) {
-      // Abre el formulario con la configuración en Culqi.settings y CulqiOptions
-
-      Culqi.settings({
+    Culqi.settings({
     title: 'Culqi Store',
     currency: 'PEN',  // Este parámetro es requerido para realizar pagos yape
     amount: 1000,  // Este parámetro es requerido para realizar pagos yape
-    //order: 'ord_live_0CjjdWhFpEAZlxlz', // Este parámetro es requerido para realizar pagos con pagoEfectivo, billeteras y Cuotéalo
+    order: 'ord_live_0CjjdWhFpEAZlxlz', // Este parámetro es requerido para realizar pagos con pagoEfectivo, billeteras y Cuotéalo
+    xculqirsaid: 'Inserta aquí el id de tu llave pública RSA',
+    rsapublickey: 'Inserta aquí tu llave pública RSA',
   });
 
   Culqi.options({
@@ -58,32 +28,12 @@
     paymentMethods: {
       tarjeta: true,
       yape: true,
-      bancaMovil: false,
+      bancaMovil: true,
       agente: true,
       billetera: true,
       cuotealo: true,
     },
   });
-
-      Culqi.open();
-      e.preventDefault();
-  })
-
-  function culqi() {
-    if (Culqi.token) {  // ¡Objeto Token creado exitosamente!
-      const token = Culqi.token.id;
-      console.log('Se ha creado un Token: ', token);
-      //En esta linea de codigo debemos enviar el "Culqi.token.id"
-      //hacia tu servidor con Ajax
-    } else if (Culqi.order) {  // ¡Objeto Order creado exitosamente!
-      const order = Culqi.order;
-      console.log('Se ha creado el objeto Order: ', order);
-
-    } else {
-      // Mostramos JSON de objeto error en consola
-      console.log('Error : ',Culqi.error);
-    }
-  };
 
   </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
